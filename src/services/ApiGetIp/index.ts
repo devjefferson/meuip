b777ec66719a2d27ce6c77de28017c79d61d885e
Jefferson fonseca <jefferson.fonseca@casion.com.br>
2022-11-02 15:45:35 -0300

    chore: no cache

--- src/services/ApiGetIp/index.ts
+++ src/services/ApiGetIp/index.ts
@@ -2,9 +2,7 @@ export async function GetIP(): Promise<string> {
   return new Promise((resolve, reject) => {
     fetch(String(process.env.NEXT_PUBLIC_API_IP), {
       method: 'GET',
-      next: {
-        revalidate: 10,
-      },
+      cache: 'no-cache',
     })
       .then((rest) => (rest.ok ? rest.json() : Promise.reject(rest.statusText)))
       .then((data) => {
